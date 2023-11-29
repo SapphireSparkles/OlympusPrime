9e292b7fac75a580af524f86588af09fd6c6686c
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2023-11-29 12:30:43 -0800

    Add files via upload

---
+++ Test/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+  <!-- basic.html -->
+  <title>basic.html</title>
+  <meta charset = "UTF-8" />
+
+
+
+
+<script src="code.js"></script>
+<script src="html2canvas.js"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="item">
+  <h3>Click here</h3>
+  <p>Lorem ipsum dolor sit amet, etc. etc.</p>
+</div>
+<div class="item">
+  <h3>Click here</h3>
+  <p>Lorem ipsum dolor sit amet, etc. etc.</p>
+</div>
+<div class="item">
+  <h3>Click here</h3>
+  <p>Lorem ipsum dolor sit amet, etc. etc.</p>
+</div>
