86e22700d4a158f8e418dde7e2e17f34f6a1cfe9
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2023-12-01 09:39:54 -0800

    recaprecap

--- Page/recap.php
+++ Page/recap.php
@@ -1,27 +1,2 @@
-<?php error_reporting(0); ?> <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Daily Recap</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-              <li class="breadcrumb-item active">Daily Recap</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
+<div align="right"><iframe src="https://temcologistics.domo.com/page/3599895" width="80%" height="550" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe> 
 
-    <!-- Main content -->
-    <section class="content">
-<?php include "Recap/index.php" ?>
-
-</section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
