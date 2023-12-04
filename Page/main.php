457b4a035651ecc169ec489b094fbbefe3d68c1b
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2023-12-04 15:36:38 -0800

    Update main.php

--- Page/main.php
+++ Page/main.php
@@ -25,7 +25,7 @@
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">Welcome: 
-                  <?php $username = getenv('USERNAME') ?: getenv('REMOTE_USER');
+                  <?php $username = getenv('INFO_VARIABLES') ?: getenv('INFO_VARIABLES');
                     echo $username; // e.g. root or www-data ?>
                 </h3>
               </div>
