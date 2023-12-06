b091c50c8c02cbd0ceb10a7474a3202857656fc3
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2023-12-06 12:35:49 -0800

    pix2

--- Page/main.php
+++ Page/main.php
@@ -50,7 +50,7 @@
                       <img class="d-block w-100" src="images/banners/BurbankMDO1.png" width="100%"  alt="5th slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="images/banners/Newark3pl1.png" width="100%"  alt="6th slide">
+                      <img class="d-block w-100" src="images/banners/Carolina3pl1.png" width="100%"  alt="6th slide">
                     </div>
                     <div class="carousel-item">
                       <img class="d-block w-100" src="images/banners/TucsonMDO1.png" width="100%"  alt="7th slide">
@@ -59,7 +59,10 @@
                       <img class="d-block w-100" src="images/banners/Pomona3PL2.png" width="100%"  alt="8th slide">
                     </div>
                     <div class="carousel-item">
-                      <img class="d-block w-100" src="images/banners/Newark3pl2.png" width="100%"  alt="9th slide">
+                      <img class="d-block w-100" src="images/banners/SanDiegoMDO2.png" width="100%"  alt="9th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Newark3pl2.png" width="100%"  alt="10th slide">
                     </div>
                   </div>
                   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
