9140acb0fc8cc5757ad415190c398c0911cb900a
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2023-12-06 17:19:21 -0800

    Update main.php

--- Page/main.php
+++ Page/main.php
@@ -34,10 +34,11 @@
                 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                   
                   <div class="carousel-inner">
+
                     <div class="carousel-item active">
                       <img class="d-block w-100" src="images/banners/Aiea3pl1.png" width="100%" alt="Zeroth slide">
                     </div>
-                    <div class="carousel-item active">
+                    <div class="carousel-item">
                       <img class="d-block w-100" src="images/banners/Carson3pl1.png" width="100%" alt="First slide">
                     </div>
                     <div class="carousel-item">
