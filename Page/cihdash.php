48014a81a6ba2dd8eba8c29ef72323ce8a7c22c0
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2024-01-03 10:52:13 -0800

    momo

--- Page/cihdash.php
+++ Page/cihdash.php
@@ -3,9 +3,11 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
+          <div class="col-12">
             <div align="right">
                 <iframe src="https://public.domo.com/embed/pages/P1vzz" width="80%" height="800" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
             </div>
+          </div> 
         </div>
       </div><!-- /.container-fluid -->
     </section>
