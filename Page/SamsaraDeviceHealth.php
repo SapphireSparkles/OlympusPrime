28e210fbc6353df31f173e1b40452f20454c657b
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2024-01-03 13:30:28 -0800

    Update SamsaraDeviceHealth.php

--- Page/SamsaraDeviceHealth.php
+++ Page/SamsaraDeviceHealth.php
@@ -1,7 +1,7 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <iframe src="https://public.domo.com/embed/pages/2RKxM"  width="100%" height="700" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
+      <iframe src="https://public.domo.com/embed/pages/2RKxM"  width="100%" height="760" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
     </section>
 </div>
   <!-- /.content-wrapper -->
