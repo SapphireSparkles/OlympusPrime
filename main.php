8890b55a30639e1257b474bbd929f47d05d44299
Mike Kennedy <128851953+SapphireSparkles@users.noreply.github.com>
2024-01-03 09:38:43 -0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,136 @@
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <!--<div class="container-fluid">-->
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+           
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      <!--</div>--><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- <div class="container-fluid"> -->
+        <center>
+          <!-- <div class="col-md-8"> -->
+          <div class="col-12 align-self-center">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Welcome!
+                  <?php //$username = getenv('INFO_VARIABLES') ?: getenv('INFO_VARIABLES'); echo $username; // e.g. root or www-data ?>
+                </h3>
+              </div>
+
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  
+                  <div class="carousel-inner">
+
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="images/banners/Aiea3pl1.png" width="100%" alt="Zeroth slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Carson3pl1.png" width="100%" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Pomona3PL1.png" width="100%"  alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/SanDiegoMDO1.png" width="100%"  alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/VegasMDO1.png" width="100%"  alt="Fourth slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/BurbankMDO1.png" width="100%"  alt="5th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Carolina3pl1.png" width="100%"  alt="6th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/TucsonMDO1.png" width="100%"  alt="7th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Marysville3PL1.png" width="100%"  alt="8th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/SpokaneMDO1.png" width="100%"  alt="9th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Newark3pl2.png" width="100%"  alt="10th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Albuquerque3pl1.png" width="100%"  alt="11th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Mesa3pl1.png" width="100%"  alt="12th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/GrandJunction3pl1.png" width="100%"  alt="13th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Flagstaff3PL1.png" width="100%"  alt="14th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/RanchoCordova3PL1.png" width="100%"  alt="15th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/TracyMDO1.png" width="100%"  alt="16th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/WestSacMDO1.png" width="100%"  alt="17th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Sisters3pl1.png" width="100%"  alt="18th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/SalemMDO1.png" width="100%"  alt="19th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/TukwilaMDO1.png" width="100%"  alt="20th slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/LaceyMDO1.png" width="100%"  alt="21st slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Beaumont3PL1.png" width="100%"  alt="22nd slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/Bakersfield3pl1.png" width="100%"  alt="23rd slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/banners/PortlandMDO1.png" width="100%"  alt="24th slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                  
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        <!--/div -->
+        
+      <!--</div>--><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
